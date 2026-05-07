8bf777811912368f5d4b7c3d8f16ce84ca71c6a5
pachecoestudante2020-afk <pachecoestudante2020@gmail.com>
2026-05-07 11:08:46 +0700

    Add missing System.IO import for MainViewModel compile fix

--- src/GenMail.Wpf/ViewModels/MainViewModel.cs
+++ src/GenMail.Wpf/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
 using GenMail.Core.Generation;
 using GenMail.Core.Models;
